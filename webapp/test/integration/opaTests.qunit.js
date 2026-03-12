8bfc62c7bb24712feca92229ef5bb6f1206df463
laurorib@outlook.com <laurorib@outlook.com>
2026-03-11 22:25:41 -0300

    Navegação no Component

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["fiori/z99bp/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"fiori/z99bp/test/unit/controller/Parceiros.controller"
+], function () {
+	"use strict";
+});
