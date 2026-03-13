50d1061ca6e1a0ab07a6f872c5a02a9c899a684a
laurorib@outlook.com <laurorib@outlook.com>
2026-03-13 20:47:35 -0300

    View Parceiros & Pesquisa

--- webapp/Component.js
+++ webapp/Component.js
@@ -27,7 +27,6 @@ sap.ui.define([
 
                 // enable routing
                 this.getRouter().initialize();
-
                 // set the device model
                 this.setModel(models.createDeviceModel(), "device");
 
